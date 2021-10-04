edc0df2bc668672c5b6a2cbdc998a5cbec39e207
dirac <767848898@qq.com>
2021-10-04 16:28:34 +0800

    add we chat authentic service

--- src/third-party/we-chat-authentic/we-chat-authentic.service.spec.ts
+++ src/third-party/we-chat-authentic/we-chat-authentic.service.spec.ts
@@ -1,18 +1,53 @@
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
+import axios, { AxiosInstance } from 'axios';
 import { WeChatAuthenticService } from './we-chat-authentic.service';
+const mockConfig: jest.Mocked<ConfigService> = {
+  get: jest.fn(),
+} as any;
+
+jest.mock('axios');
 
 describe('WeChatAuthenticService', () => {
   let service: WeChatAuthenticService;
+  const mockAxios: jest.Mocked<AxiosInstance> = axios as any;
 
   beforeEach(async () => {
+    jest.clearAllMocks();
     const module: TestingModule = await Test.createTestingModule({
+      imports: [ConfigModule],
       providers: [WeChatAuthenticService],
-    }).compile();
+    })
+      .overrideProvider(ConfigService)
+      .useValue(mockConfig)
+      .compile();
 
     service = module.get<WeChatAuthenticService>(WeChatAuthenticService);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('getUserInfoByAuthCode', async () => {
+    const url = 'https://api.weixin.qq.com/sns/jscode2session';
+    const params = {
+      appid: 'test_id',
+      secret: 'test_secret',
+      js_code: 'test_code',
+      grant_type: 'authorization_code',
+    };
+    const tResult = {
+      openid: 'testid',
+      errcode: 0,
+    };
+    mockConfig.get.mockReturnValueOnce(params.appid);
+    mockConfig.get.mockReturnValueOnce(params.secret);
+    mockAxios.get.mockResolvedValue({ data: tResult });
+
+    const result = await service.getUserInfoByAuthCode(params.js_code);
+
+    expect(axios.get).toBeCalledWith(url, { params });
+    expect(result).toEqual(tResult);
   });
 });
--- src/third-party/we-chat-authentic/we-chat-authentic.service.ts
+++ src/third-party/we-chat-authentic/we-chat-authentic.service.ts
@@ -1,12 +1,24 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import axios from 'axios';
 import { UserInfo } from './interface/user-info.interface';
 
 @Injectable()
 export class WeChatAuthenticService {
   constructor(private readonly config: ConfigService) {}
 
-  getUserInfoByAuthCode(authCode: string): UserInfo {
-    throw new Error('not implementation');
+  async getUserInfoByAuthCode(authCode: string): Promise<UserInfo> {
+    const { data: result } = await axios.get(
+      'https://api.weixin.qq.com/sns/jscode2session',
+      {
+        params: {
+          appid: this.config.get('WE_CHAT_APPID'),
+          secret: this.config.get('WE_CHAT_SECRET'),
+          js_code: authCode,
+          grant_type: 'authorization_code',
+        },
+      },
+    );
+    return result;
   }
 }
